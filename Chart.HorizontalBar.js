329a17fc07c523f10c29615fad8a0af2c523e475
coolbombom <cool_bombom@hotmail.com>
2016-01-15 16:47:52 +0100

    changed default stacked option
    changed default stacked option

--- Chart.HorizontalBar.js
+++ Chart.HorizontalBar.js
@@ -41,7 +41,7 @@
 		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 
                 //String - axis to stack bars on
-		stacked : 'none'
+		stacked : 'y'
 	};
 
   Chart.HorizontalRectangle = Chart.Element.extend({
