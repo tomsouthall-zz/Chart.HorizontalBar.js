bdc5f508cd9c47ee97c2decd1f906b1b94f056a7
coolbombom <cool_bombom@hotmail.com>
2016-01-21 11:58:58 +0100

    Update Chart.HorizontalBar.js

--- Chart.HorizontalBar.js
+++ Chart.HorizontalBar.js
@@ -509,10 +509,10 @@
 						    bar.startX = 0;
 						    bar._saved.left = Math.round(this.scale.xScalePaddingLeft);
 						}
-						if (this.datasets[datasetIndex + 1] && this.options.stacked == "x") {
-						      this.datasets[datasetIndex + 1].bars[index].startX = (bar.startX - Math.round(this.scale.xScalePaddingLeft)) + this.scale.calculateXInvertXY(bar.value); 
-						      this.datasets[datasetIndex + 1].bars[index]._saved.left = (this.datasets[datasetIndex + 1].bars[index].startX - ((((this.scale.calculateXInvertXY(bar.value) + bar.startX) - bar._saved.x) * ease) + bar._saved.x))/5;
-						      if (!this.datasets[datasetIndex + 1].bars[index]._saved.left) this.datasets[datasetIndex + 1].bars[index]._saved.left = 0;  
+						if (this.datasets[datasetIndex + 1] && this.options.stacked == "x") {	
+							this.datasets[datasetIndex + 1].bars[index].startX = ((bar.startX - Math.round(this.scale.xScalePaddingLeft)) + this.scale.calculateXInvertXY(bar.value)); 
+							this.datasets[datasetIndex + 1].bars[index]._saved.left = ((this.datasets[datasetIndex + 1].bars[index].startX - ((((this.scale.calculateXInvertXY(bar.value) + bar.startX) - bar._saved.x) * ease)))/100) + Math.round(this.scale.xScalePaddingLeft);
+							if (!this.datasets[datasetIndex + 1].bars[index]._saved.left) this.datasets[datasetIndex + 1].bars[index]._saved.left = Math.round(this.scale.xScalePaddingLeft);   
 						}
 						//Transition then draw
 						bar.transition({
